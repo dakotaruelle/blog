8b0176b559b3571df76f3e85c6b37d880fa6c21c
Dakota Ruelle <17259752+dakotaruelle@users.noreply.github.com>
2020-11-07 17:18:00 -0600

    Update main layout and app bar Add toggle for light/dark themes

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,16 +1,19 @@
 <template>
-  <article>
-    <nuxt-content :document="article" />
-  </article>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <article>
+        <nuxt-content :document="article" />
+      </article>
+    </v-col>
+  </v-row>
 </template>
 
-
 <script>
-  export default {
-    async asyncData({ $content, params }) {
-      const article = await $content('articles', params.slug).fetch()
+export default {
+  async asyncData({ $content, params }) {
+    const article = await $content('articles', params.slug).fetch()
 
-      return { article }
-    }
-  }
+    return { article }
+  },
+}
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,14 @@
 <template>
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to my website
-        </v-card-title>
-        <v-card-text>
-          <p>
-            This is the home page
-          </p>
-        </v-card-text>
-      </v-card>
+      <h1>Welcome to my website</h1>
+      <p>This is the home page</p>
     </v-col>
   </v-row>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
+import Vue from 'vue'
 
-export default {
-  components: {
-    Logo,
-    VuetifyLogo,
-  },
-}
+export default Vue.extend({})
 </script>

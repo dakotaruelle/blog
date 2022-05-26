66757db6d1c8c6bf7783e019c36b9f62d842866f
Dakota Ruelle <17259752+dakotaruelle@users.noreply.github.com>
2022-05-26 12:34:50 -0500

    Replace ResponsiveAppBar with AppBar that hides when scrolled

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,12 +1,52 @@
-import { ResponsiveAppBar } from './responsive-app-bar';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import CssBaseline from '@mui/material/CssBaseline';
+import useScrollTrigger from '@mui/material/useScrollTrigger';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Slide from '@mui/material/Slide';
+import CottageIcon from '@mui/icons-material/Cottage';
 
-export function Layout({ children }: { children: React.ReactNode }) {
+interface Props {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window?: () => Window;
+  children: React.ReactElement;
+}
+
+function HideOnScroll(props: Props) {
+  const { children, window } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+  });
+
   return (
-    <>
-      <div>
-        <ResponsiveAppBar />
-        <main>{children}</main>
-      </div>
-    </>
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  );
+}
+
+export function Layout(props: Props) {
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <HideOnScroll {...props}>
+        <AppBar>
+          <Toolbar>
+            <CottageIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          </Toolbar>
+        </AppBar>
+      </HideOnScroll>
+      <Toolbar />
+      <Container>{props.children}</Container>
+    </React.Fragment>
   );
 }

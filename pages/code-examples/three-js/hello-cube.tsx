a136484d401e2deae37f312aa321691373a151cc
Dakota Ruelle <17259752+dakotaruelle@users.noreply.github.com>
2022-05-28 11:43:03 -0500

    Update HelloCubePage with description

--- pages/code-examples/three-js/hello-cube.tsx
+++ pages/code-examples/three-js/hello-cube.tsx
@@ -18,6 +18,27 @@ export default function HelloCubePage() {
   return (
     <>
       <div className="stackblitz-container">
+        <h1>Three JS - Hello Cube</h1>
+        <p>The hello world of Three JS, a basic scene with a simple cube</p>
+        <p>
+          This cube uses a{' '}
+          <a href="https://threejs.org/docs/index.html#api/en/geometries/BoxGeometry">
+            BoxGeomerty
+          </a>{' '}
+          with a{' '}
+          <a href="https://threejs.org/docs/index.html#api/en/materials/MeshBasicMaterial">
+            MeshBasicMaterial
+          </a>
+          . It utilizes a{' '}
+          <a href="https://threejs.org/docs/index.html?q=pers#api/en/cameras/PerspectiveCamera">
+            PerspectiveCamera
+          </a>
+          ,{' '}
+          <a href="https://threejs.org/docs/index.html?q=orbit#examples/en/controls/OrbitControls">
+            OrbitControls
+          </a>{' '}
+          for interactivity, and resizes when the window changes.
+        </p>
         <div id="stackblitz-embed"></div>
       </div>
     </>
